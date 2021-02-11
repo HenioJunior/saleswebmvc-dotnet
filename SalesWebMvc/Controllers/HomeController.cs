42a927ebb4e46662422a1c1ad548d0cc12622ce4
Henio Junior <henioajunior@gmail.com>
2021-02-11 20:36:03 -0300

    First controller and Razor pages tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,8 +17,9 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
-
+            ViewData["Message"] = "Sales Web Mvc";
+            ViewData["Name"] = "Henio Junior";
+            
             return View();
         }
 
